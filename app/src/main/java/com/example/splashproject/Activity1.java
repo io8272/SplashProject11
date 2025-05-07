64c772cdae655a34c34ece66867f2b55a56aacb1
io8272 <io8272@naver.com>
2025-05-08 00:43:57 +0900

    Initial commit

---
+++ app/src/main/java/com/example/splashproject/Activity1.java
@@ -0,0 +1,39 @@
+package com.example.splashproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity1 extends AppCompatActivity {
+
+    private static final int SPLASH_TIMEOUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main1), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+
+            @Override
+            public void run(){
+                Intent intent = new Intent(Activity1.this, Activity2.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
+    }
+}
