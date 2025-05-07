9178851750bfba1d0592a33c02b7b6b9915c796b
io8272 <io8272@naver.com>
2025-05-08 02:24:55 +0900

    5월 1일 수업

--- app/src/main/java/com/example/splashproject/Activity2.java
+++ app/src/main/java/com/example/splashproject/Activity2.java
@@ -1,14 +1,30 @@
 package com.example.splashproject;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Activity2 extends AppCompatActivity {
+
+    String tag = "LoginChecker";
+    ActivityResultLauncher<Intent> launcher;
+
+    private EditText j_emailEditText, j_passwordEditText;
+    private TextView j_statusText;
+    private Button buttonlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +34,39 @@ public class Activity2 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        j_emailEditText = findViewById(R.id.EditText1);
+        j_passwordEditText = findViewById(R.id.EditText2);
+        j_statusText = findViewById(R.id.statusText);
+        buttonlogin = findViewById(R.id.LoginButton);
+
+        buttonlogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //아이디와 비밀번호를 String변수에 대입
+                String email = j_emailEditText.getText().toString();
+                String password = j_passwordEditText.getText().toString();
+
+                Log.d(tag, "입력 아이디: " + email);
+                Log.d(tag, "입력 비밀번호: " + password);
+
+                Intent intent = new Intent(Activity2.this, Activity3.class);
+                intent.putExtra("ID", email);
+                intent.putExtra("Password", password);
+
+                //런처 실행
+                launcher.launch(intent);
+            }
+        });
+
+        //launcher
+
+        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode()== Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        j_statusText.setText(data.getStringExtra("status"));
+                }
         });
     }
 }
